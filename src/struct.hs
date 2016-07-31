bcdcba9445b5fe6d35dd434f9c6c67e3c517c64d
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-07-31 03:54:50 +0100

    Update benchmarking module

--- src/struct.hs
+++ src/struct.hs
@@ -1,191 +1,24 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns  #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main where
 
-import           System.Environment          (getArgs, withArgs)
-import           Control.DeepSeq             (NFData)
+import           System.Environment                    (getArgs, withArgs)
+import           Control.DeepSeq                       (NFData)
 import           Criterion.Main
-import           Data.ByteString.Char8       (ByteString, pack)
-import           Data.Hashable               (Hashable)
-import qualified Data.Map.Lazy         as ML
-import qualified Data.Map.Strict       as MS
-import qualified Data.IntMap.Lazy      as IL
-import qualified Data.IntMap.Strict    as IS
-import qualified Data.HashMap.Lazy     as HL
-import qualified Data.HashMap.Strict   as HS
-import           System.Random               (mkStdGen, randomRs)
-
-{-class IsMap map where
-    -- | In some cases, 'MapValue' and 'Element' will be different, e.g., the
-    -- 'IsMap' instance of associated lists.
-    type MapKey map
-
-    type MapValue map
-
-    -- | Look up a value in a map with a specified key.
-    lookup       :: MapKey map -> map -> Maybe (MapValue map)
-
-    -- | Insert a key-value pair into a map.
-    insert    :: MapKey map -> MapValue map -> map -> map
-
-    -- | Delete a key-value pair of a map using a specified key.
-    delete    :: MapKey map -> map -> map
-
-    alter     :: (Maybe (MapValue map) -> Maybe (MapValue map))
-              -> MapKey map
-              -> map
-              -> map
-
-    -- | Get the union of two containers.
-    union        :: map -> map -> map
-
-    -- | Get the difference of two containers.
-    difference   :: map -> map -> map
-
-    -- | Get the intersection of two containers.
-    intersection :: map -> map -> map
-
-    empty        :: map
-
-    fromList       :: [(MapKey map, MapValue map)] -> map
-
-    -- | Convert a map to a list of key-value pairs.
-    toList      :: map -> [(MapKey map, MapValue map)]
-
-{-instance Ord k => IsMap (ML.Map k v) where
-    type MapKey (ML.Map k v) = k
-    type MapValue (ML.Map k v) = v
-    lookup = ML.lookup
-    {-# INLINE lookup #-}
-    insert = ML.insert
-    {-# INLINE insert #-}
-    alter = ML.alter
-    {-# INLINE alter#-}
-    delete = ML.delete
-    {-# INLINE delete#-}
-    union = ML.union
-    {-# INLINE union #-}
-    difference = ML.difference
-    {-# INLINE difference #-}
-    intersection = ML.intersection
-    {-# INLINE intersection #-}
-    fromList = ML.fromList
-    {-# INLINE fromList #-}
-    toList = ML.toList
-    {-# INLINE toList #-}-}
-
-instance Ord k => IsMap (MS.Map k v) where
-    type MapKey (MS.Map k v) = k
-    type MapValue (MS.Map k v) = v
-    lookup = MS.lookup
-    {-# INLINE lookup #-}
-    insert = MS.insert
-    {-# INLINE insert #-}
-    alter = MS.alter
-    {-# INLINE alter#-}
-    delete = MS.delete
-    {-# INLINE delete#-}
-    union = MS.union
-    {-# INLINE union #-}
-    difference = MS.difference
-    {-# INLINE difference #-}
-    intersection = MS.intersection
-    {-# INLINE intersection #-}
-    empty = MS.empty
-    fromList = MS.fromList
-    {-# INLINE fromList #-}
-    toList = MS.toList
-    {-# INLINE toList #-}
-
-{-instance IsMap (IL.IntMap value) where
-    type MapKey (IL.IntMap value) = Int
-    type MapValue (IL.IntMap value) = value
-    lookup = IL.lookup
-    {-# INLINE lookup #-}
-    insert = IL.insert
-    {-# INLINE insert #-}
-    alter = IL.alter
-    {-# INLINE alter #-}
-    delete = IL.delete
-    {-# INLINE delete#-}
-    union = IL.union
-    {-# INLINE union #-}
-    difference = IL.difference
-    {-# INLINE difference #-}
-    intersection = IL.intersection
-    {-# INLINE intersection #-}
-    fromList = IL.fromList
-    {-# INLINE fromList #-}
-    toList = IL.toList
-    {-# INLINE toList #-}-}
-
-instance IsMap (IS.IntMap value) where
-    type MapKey (IS.IntMap value) = Int
-    type MapValue (IS.IntMap value) = value
-    lookup = IS.lookup
-    {-# INLINE lookup #-}
-    insert = IS.insert
-    {-# INLINE insert #-}
-    alter = IS.alter
-    {-# INLINE alter#-}
-    delete = IS.delete
-    {-# INLINE delete#-}
-    union = IS.union
-    {-# INLINE union #-}
-    difference = IS.difference
-    {-# INLINE difference #-}
-    intersection = IS.intersection
-    {-# INLINE intersection #-}
-    empty = IS.empty
-    fromList = IS.fromList
-    {-# INLINE fromList #-}
-    toList = IS.toList
-    {-# INLINE toList #-}
-
-{-instance (Eq key,Hashable key) => IsMap (HL.HashMap key value) where
-    type MapKey (HL.HashMap key value) = key
-    type MapValue (HL.HashMap key value) = value
-    lookup = HL.lookup
-    {-# INLINE lookup #-}
-    insert = HL.insert
-    {-# INLINE insert #-}
-    alter = HL.alter
-    {-# INLINE alter#-}
-    delete = HL.delete
-    {-# INLINE delete#-}
-    union = HL.union
-    {-# INLINE union #-}
-    difference = HL.difference
-    {-# INLINE difference #-}
-    intersection = HL.intersection
-    {-# INLINE intersection #-}
-    fromList = HL.fromList
-    {-# INLINE fromList #-}
-    toList = HL.toList
-    {-# INLINE toList #-}-}
-
-instance (Eq key, Hashable key) => IsMap (HS.HashMap key value) where
-    type MapKey (HS.HashMap key value) = key
-    type MapValue (HS.HashMap key value) = value
-    lookup = HS.lookup
-    {-# INLINE lookup #-}
-    insert = HS.insert
-    {-# INLINE insert #-}
-    alter = HS.alter
-    {-# INLINE alter#-}
-    delete = HS.delete
-    {-# INLINE delete#-}
-    union = HS.union
-    {-# INLINE union #-}
-    difference = HS.difference
-    {-# INLINE difference #-}
-    intersection = HS.intersection
-    {-# INLINE intersection #-}
-    empty = HS.empty
-    fromList = HS.fromList
-    {-# INLINE fromList #-}
-    toList = HS.toList
-    {-# INLINE toList #-}-}
+import           Data.ByteString.Char8                 (ByteString, pack)
+import           Data.Hashable                         (Hashable)
+import qualified Data.Map.Lazy                   as ML
+import qualified Data.Map.Strict                 as MS
+import qualified Data.IntMap.Lazy                as IL
+import qualified Data.IntMap.Strict              as IS
+import qualified Data.HashMap.Lazy               as HL
+import qualified Data.HashMap.Strict             as HS
+import qualified Data.Edison.Assoc.AssocList     as AL
+import qualified Data.Edison.Assoc.PatriciaLoMap as PM
+import qualified Data.Edison.Assoc.StandardMap   as SM
+import qualified Data.Edison.Assoc.TernaryTrie   as TT
+import           System.Random                         (mkStdGen, randomRs)
 
 ascBS :: Int -> Int -> [ByteString]
 ascBS strlen num = map pack $ ascS strlen num
@@ -347,6 +180,13 @@ bench_criterion bnch =
 
 create fromL l = fromL $ zip l l
 
+{-benchMaker :: NFData b =>
+              Int
+           -> (Int -> [a])
+           -> (Int -> [a])
+           -> ([(a,a)] -> t)
+           -> (String, ([a], t, t, t) -> b)
+           -> [Benchmark]-}
 benchMaker len asce rand fromL benchmark =
     let lenarg = show len
         asc_list = asce len
@@ -373,9 +213,9 @@ benchMaker len asce rand fromL benchmark =
      odd [x]      = [x]
      odd (x:_:xs) = x : odd xs
 
-intBench num name fromL method method_name =
+intBench (num, asc_, rnd_) name fromL method method_name =
     bgroup name $
-    benchMaker num asc rnd fromL ((,) method_name method)
+    benchMaker num asc_ rnd_ fromL ((,) method_name method)
 
 ins = "insert"
 lkup = "lookup"
@@ -488,30 +328,38 @@ foldListBenches num =
 
 x & f = f x
 
+benchDecider benchfun =
+    case benchfun of
+        "insert" -> do
+            insertBenches
+        "lookup" -> do
+            lookupBenches
+        "delete" -> do
+            deleteBenches
+        "alter_insert" -> do
+            alterInsBenches
+        "alter_delete" -> do
+            alterDelBenches
+        "union" -> do
+            unionBenches
+        "difference" -> do
+            differenceBenches
+        "intersection" -> do
+            intersectionBenches
+        "fold" -> do
+            foldListBenches
+
 main = do
-  n <- getLine
-  benchtype <- getLine
-  benchfun <- getLine
+  n : benchtype : benchfun : args <- getArgs
   let num = (read n) :: Int
+  {-let pair@(num ,(ascend, random)) = (,) <$> ((read n) :: Int) <*> helper
+      helper = case args of
+                  [] -> (asc,rnd)
+                  [strl] -> let strlen = (read strl) :: Int
+                            in case benchtype of
+                                   "bytestring" -> (ascBS strlen, rndBS strlen)-}
   case benchtype of
       "int" -> do
-          num &
-              case benchfun of
-                  "insert" -> do
-                      insertBenches
-                  "lookup" -> do
-                      lookupBenches
-                  "delete" -> do
-                      deleteBenches
-                  "alter insert" -> do
-                      alterInsBenches
-                  "alter delete" -> do
-                      alterDelBenches
-                  "union" -> do
-                      unionBenches
-                  "difference" -> do
-                      differenceBenches
-                  "intersection" -> do
-                      intersectionBenches
-                  "fold" -> do
-                      foldListBenches
+          (num, asc, rnd) & benchDecider benchfun
+      {-"bytestring" -> do
+          (num, ascBS, rndBS) & benchDecider benchfun-}
